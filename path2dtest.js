b5c744e2be6e7a24ede549d77cf564e1505f2a9d
Joshua Kim <kndewat@gmail.com>
2024-10-14 23:14:39 +0900

    Initial commit

--- path2dtest.js
+++
@@ -1,7 +0,0 @@
-// testPath2D.js
-const canvas = require('canvas');
-
-const pathData = 'M10 10 h 80 v 80 h -80 Z';
-const path = new canvas.Path2D(pathData);
-
-console.log('Path2D is working:', path !== undefined);
